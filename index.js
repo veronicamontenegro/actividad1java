fbe089526f9beb0bbcef687a91081f78176afe3f
veronica montenegro <veronicanahir@hotmail.com>
2022-10-24 23:32:46 -0300

    primera actividad java

---
+++ index.js
@@ -0,0 +1,25 @@
+const mensaje = "¡MIRÁ MAMÁAA, ESTOY PROGRAMANDO!"
+console.log(mensaje)
+
+const numeroPar = 18
+const numeroImpar = 35
+console.log( numeroPar + numeroImpar)
+
+prompt("Ingrese su nombre")
+const nombreDelUsuario = prompt
+console.log(nombreDelUsuario)
+
+alert("Bienvenide a nuestra primera web -usuario-")
+const mensajeDeBienvenida = alert
+console.log(mensajeDeBienvenida)
+
+let numeroCinco = 5
+console.log(numeroCinco * numeroImpar)
+
+const miNombre = "Veronica"
+console.log(miNombre)
+
+console.log(numeroCinco -100)
+
+console.log(typeof numeroImpar)
+console.log(typeof nombreDelUsuario)
